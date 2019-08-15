85ecaf2a06225d2842c84af1185e126a4e4a3704
wangliang <850598163@qq.com>
2019-08-15 20:09:36 +0800

    版本小升级 使用类型优化

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
 export * from './lib/decorator-util';
 export * from './lib/decorator-factory/decorator-factory-builder';
+export * from './lib/bean/decorator-factory';
--- lib/bean/metadata-info.ts
+++ lib/bean/metadata-info.ts
@@ -1,5 +1,7 @@
+import {DecoratorFactory} from "./decorator-factory";
+
 export type MetadataInfo<V> = {
     metadataKey: symbol;
     // 为了保存类型 实际为空
     __metadataValueType: V;
-}
+} & DecoratorFactory;
